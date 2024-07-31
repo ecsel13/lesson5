4b64f0fdb215f64bb804cb208e7bb90d5a15235c
ecsel13 <ecsel13@mail.ru>
2024-07-31 15:25:56 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,9 +1,9 @@
 immutable_var = 1, 'cherry', 3, 45, True
 print(immutable_var)
 # immutable_var[0] = 200 сам объект кортежа не изменяем, изменение могут происходить только внутри объекта
-immutable_list = ['apple', 'cherry', 2, 34, False]
-print(immutable_list)
-immutable_list.append(1)
-print(immutable_list)
-immutable_list.remove('apple')
-print(immutable_list)
+mutable_list = ['apple', 'cherry', 2, 34, False]
+print(mutable_list)
+mutable_list.append(1)
+print(mutable_list)
+mutable_list.remove('apple')
+print(mutable_list)
